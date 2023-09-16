8d7a5f26d5398fface820665d568392add6e50b5
Kevin Bockman <ops@mensunderwearguide.com>
2023-09-16 10:17:36 -0500

    50 VU for 10m

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '5m',
+  vus: 50,
+  duration: '10m',
 };
 
 export default function () {
