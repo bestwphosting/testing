9a77974bc848346a87da0f9855d687e8ab91a357
Kevin Bockman <ops@mensunderwearguide.com>
2023-09-17 17:32:56 -0500

    up back to 50vu

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 1,
-  duration: '1s',
+  vus: 50,
+  duration: '5m',
 };
 
 export default function () {
@@ -15,7 +15,10 @@ export function handleSummary(data) {
   const durations = data.metrics.http_req_duration.values;
   const fails = data.metrics.http_req_failed.values;
 
+  // summary fails and passes should really be the other way around
+  // https://github.com/grafana/k6/issues/2306
   return {
-    'summary.csv': `${durations.min},${durations.max},${durations.avg},${fails.fails},${fails.passes},${fails.rate}`,
+    'summary.csv': `http_req_duration_min,http_req_duration_max,http_req_duration_avg,http_req_failed_fails,http_req_failed_passes,http_req_failed_rate
+${durations.min},${durations.max},${durations.avg},${fails.fails},${fails.passes},${fails.rate}`,
   };
 }
