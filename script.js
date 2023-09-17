4af95e6bc49daef596c3ca63486e65dbbde7b632
Kevin Bockman <ops@mensunderwearguide.com>
2023-09-17 15:59:00 -0500

    script summary

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 50,
-  duration: '10m',
+  vus: 1,
+  duration: '1s',
 };
 
 export default function () {
   http.get(`${__ENV.URL}`);
   sleep(1);
+}
+
+export function handleSummary(data) {
+  const durations = data.metrics.http_req_duration.values;
+  const fails = data.metrics.http_req_failed.values;
+
+  return {
+    'summary.csv': `${durations.min},${durations.max},${durations.avg},${fails.fails},${fails.passes},${fails.rate}`,
+  };
 }
