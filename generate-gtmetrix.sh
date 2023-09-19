38bd84ac0380ceae25b2cf7fb0af2ed9299146f2
Kevin Bockman <ops@mensunderwearguide.com>
2023-09-19 07:56:19 -0500

    final changes for generate gtmetrix

--- generate-gtmetrix.sh
+++ generate-gtmetrix.sh
@@ -1,52 +1,79 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 BASE_CURL="curl -s -u ${GTMETRIX_API_KEY}:"
 BASE_API_URL=https://gtmetrix.com/api/2.0
 HOST=$1
-PAGE=$2
-[ -z "$PAGE" ] && PAGE="/ecuador-language-tips-learn-before-you-go-large/"
-LOCATIONS_DESKTOP=(1)
-#LOCATIONS_MOBILE=(1 9 11 6 21 2 15 22 5 17 3 7 18)
+UNOPT_PAGE="/ecuador-language-tips-learn-before-you-go-large/"
+OPT_PAGE="/ecuador-language-tips-learn-before-you-go/"
+LOCATIONS_MOBILE=(9 11 6 21 2 15 22 5 17 3 7 18)
 
 $BASE_CURL ${BASE_API_URL}/status
 
-for location in "${LOCATIONS_DESKTOP[@]}"; do
-	output=$($BASE_CURL -X POST -H Content-Type:application/vnd.api+json \
+function run_test() {
+	local location=$1
+	local optimization=$2
+	local type=$3
+
+	local adtl_json
+	local page
+
+	if [ "$optimization" == "opt" ]; then
+		page=$OPT_PAGE
+	else
+		page=$UNOPT_PAGE
+	fi
+
+	if [ "$type" == "mobile" ]; then
+		adtl_json="\"throttle\": \"15000/10000/100\", \"simulate_device\": \"iphone_13\","
+	fi
+
+	echo "Requesting report for $HOST on $page OPT: $optimization TYPE: $type LOCATION: $location"
+	local output=$($BASE_CURL -X POST -H Content-Type:application/vnd.api+json \
 	    ${BASE_API_URL}/tests \
 	    -d "
 	{
 	  \"data\": {
 	    \"type\": \"test\",
 	    \"attributes\": {
-	      \"url\":      \"https://${HOST}${PAGE}\",
+	      \"url\":      \"https://${HOST}${page}\",
 	      \"location\": \"${location}\",
 	      \"browser\":  \"3\",
 	      \"report\":   \"lighthouse\",
 	      \"retention\": 1,
 	      \"adblock\":  0,
 	      \"video\": 0,
+	      ${adtl_json}
 	      \"stop_onload\": 0
 	    }
 	  }
   	}")
 	echo $output | jq .
-	test_link=$(echo $output | jq -r .links.self)
-	[ "$test_link" == "null" ] && continue
+	local test_link=$(echo $output | jq -r .links.self)
+	[ "$test_link" == "null" ] && echo "Null test link. Giving up on this one." && return
 
 	# Wait for report to finish
-	test_state=""
+	local test_state=""
 	while [ "$test_state" != "completed" ] && [ "$test_state" != "error" ]; do
-		test_output=$($BASE_CURL $test_link)
-		test_state=$(echo $test_output | jq -r .data.attributes.state)
+		local test_output=$($BASE_CURL $test_link)
+		local test_state=$(echo $test_output | jq -r .data.attributes.state)
+		echo "Test state: $test_state"
 		if [ "$test_state" == "completed" ]; then
-			report_link=$(echo $test_output | jq -r .data.links.report)
-			report_download=$($BASE_CURL $report_link | jq -r .data.links.report_pdf)
-			$BASE_CURL -o ${HOST}-${location}-report.pdf -L $report_download
+			local report_link=$(echo $test_output | jq -r .data.links.report)
+			local report_download=$($BASE_CURL $report_link | jq -r .data.links.report_pdf)
+			$BASE_CURL -o ${HOST}-${type}-${optimization}-${location}-GTMetrix.pdf -L $report_download
 			continue
 		elif [ "$test_state" == "error" ]; then
-			error=$(echo $test_output | jq -r .data.attributes.error)
+			echo $test_output | jq -r .data.attributes.error
 			continue
 		fi
 		sleep 5
 	done
+}
 
-done
+run_test 1 "unopt" "desktop" 
+run_test 1 "opt" "desktop"
+run_test 1 "unopt" "mobile"
+run_test 1 "opt" "mobile"
+
+for id in "${LOCATIONS_MOBILE[@]}"; do
+       run_test $id "unopt" "mobile"
+done       
