63faead86f5d4c30f52a40d25052512f5478266f
Kevin Bockman <ops@mensunderwearguide.com>
2023-10-01 14:32:48 -0500

    local-script: add support for db port

--- local-script.sh
+++ local-script.sh
@@ -23,6 +23,11 @@ if [ -z "$MYSQL_PWD" ]; then
 	DB_USER=$(grep DB_USER ${PUBLIC_HTML_DIR}/wp-config.php | grep define | cut -d"'" -f4)
 	DB_HOST=$(grep "DB_HOST'" ${PUBLIC_HTML_DIR}/wp-config.php | grep define | cut -d"'" -f4)
 	DB_PASSWORD=$(grep DB_PASSWORD ${PUBLIC_HTML_DIR}/wp-config.php | grep define | cut -d"'" -f4)
+
+	if [[ "$DB_HOST" =~ : ]]; then
+		DB_PORT="--port=$(echo $DB_HOST | cut -d ':' -f2)"
+		DB_HOST=$(echo $DB_HOST | cut -d ':' -f1)
+	fi
 else
 	# Pressable (others are in env)
 	DB_HOST=127.0.0.1
@@ -47,8 +52,8 @@ cp template.bestwp.hosting/robots.txt ${PUBLIC_HTML_DIR}/
 echo "Updating database..."
 TABLE_PREFIX=$(grep table_prefix template.bestwp.hosting/wp-config.php | cut -d"'" -f2)
 sed -i "s/^\$table_prefix.*/\$table_prefix = '${TABLE_PREFIX}';/" ${PUBLIC_HTML_DIR}/wp-config.php
-mysql --user=$DB_USER --password=$DB_PASSWORD --host=$DB_HOST $DB_NAME < remove-all-tables.sql
+mysql --user=$DB_USER --password=$DB_PASSWORD --host=$DB_HOST $DB_PORT $DB_NAME < remove-all-tables.sql
 #sed -i "s/template.bestwp.hosting/$WEBSITE_HOSTNAME/g" 20230920-template.sql
-mysql --user=$DB_USER --password=$DB_PASSWORD --host=$DB_HOST $DB_NAME < template.sql
+mysql --user=$DB_USER --password=$DB_PASSWORD --host=$DB_HOST $DB_PORT $DB_NAME < template.sql
 sed -i "s/%%HOST%%/$WEBSITE_HOSTNAME/g" update-url.sql
-mysql --user=$DB_USER --password=$DB_PASSWORD --host=$DB_HOST $DB_NAME < update-url.sql 
+mysql --user=$DB_USER --password=$DB_PASSWORD --host=$DB_HOST $DB_PORT $DB_NAME < update-url.sql
